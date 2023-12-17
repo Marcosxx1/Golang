b488d817e9e16c13097328174e151fa5708940b4
marcos <marcosxx1.ma@gmail.com>
2023-12-17 10:09:17 -0400

    Coreções de ponteiros e endereçamento

--- model/compras-mes.go
+++ model/compras-mes.go
@@ -1,11 +1,43 @@
+/*
+	package model
+
+import "time"
+
+	type ComprasMes struct {
+		DataCompra       time.Time
+		Mercado          Mercado
+		ItemsParaComprar []ItemsCompra
+	}
+
+func IniciaCompraMes() *ComprasMes {
+
+		localDeCompra := &Mercado{
+			Endereco: "Rua do Mercado, 123",
+			Nome:     "Mercado Mercadil",
+		}
+
+		itemsCompraDaCompra := &ItemsCompra{
+			Nome:       "Certamente não é manteiga",
+			Quantidade: 1,
+		}
+
+		comprasDoMes := &ComprasMes{
+			DataCompra:       time.Date(2023, 11, 23, 0, 0, 0, 0, time.Local),
+			Mercado:          *localDeCompra,                      //Aponta para o valor de Mercado criado localmente
+			ItemsParaComprar: []ItemsCompra{*itemsCompraDaCompra}, //Aponta para o valor de ItemsCompr criado localmente
+		}
+
+		return comprasDoMes
+	}
+*/
 package model
 
 import "time"
 
 type ComprasMes struct {
 	DataCompra       time.Time
-	Mercado          Mercado
-	ItemsParaComprar []ItemsCompra
+	Mercado          *Mercado
+	ItemsParaComprar []*ItemsCompra
 }
 
 func IniciaCompraMes() *ComprasMes {
@@ -22,8 +54,8 @@ func IniciaCompraMes() *ComprasMes {
 
 	comprasDoMes := &ComprasMes{
 		DataCompra:       time.Date(2023, 11, 23, 0, 0, 0, 0, time.Local),
-		Mercado:          *localDeCompra,
-		ItemsParaComprar: []ItemsCompra{*itemsCompraDaCompra},
+		Mercado:          localDeCompra,                       //Aponta para uma instancia existente de Mercado
+		ItemsParaComprar: []*ItemsCompra{itemsCompraDaCompra}, // Aoibta oara uma instancia existente de ItemsCompra
 	}
 
 	return comprasDoMes
