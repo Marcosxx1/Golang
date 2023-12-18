acb15ea92fee307b5c4eafc22257aedbd0755a6f
marcos <marcosxx1.ma@gmail.com>
2023-12-17 21:42:17 -0400

    panic, defer e recover

--- main.go
+++ main.go
@@ -1,10 +1,31 @@
 package main
 
-import "Golang/mains"
+import (
+	"fmt"
+	"os"
+)
+
+func MostraTexto() {
+	fmt.Println("Finalizando")
+}
 
 func main() {
 
-	mains.Structs()
-	mains.Interfaces()
-	mains.Generics()
+	// Cria o arquivo
+	file, err := os.Create("./setting.go")
+
+	// Verifica por erros
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = file.Write([]byte("teste"))
+	if err != nil {
+		panic(err)
+	}
+
+	// Finaliza a E/S de arquivos
+	defer file.Close() // defer =
+	// Depois mostra o texto
+	defer MostraTexto()
 }
