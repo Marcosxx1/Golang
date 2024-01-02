d5bb5910173d6998e8a02be9baf57d9c6984fdfe
marcos <marcosxx1.ma@gmail.com>
2024-01-02 07:51:30 -0400

    continuando validação

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -15,7 +15,7 @@ type Campaign struct {
 	Name      string    `validate:"min=5,max=25"`
 	CreatedAt time.Time `validate:"required"`
 	Content   string    `validate:"min=5,max=1024"`
-	Contacts  []Contact `validate:"min=1,max=25"`
+	Contacts  []Contact `validate:"min=1,max=25,dive"`
 }
 
 func NewCampaign(name string, content string, emails []string) (*Campaign, error) {
--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"Golang/internal/domain/campaign"
+
+	"github.com/go-playground/validator/v10"
+)
+
 func main() {
 
+	contacts := []campaign.Contact{{Email: ""}}
+	campaign := campaign.Campaign{
+		Name:     "Campanha Teste",
+		Contacts: contacts,
+	}
+	validate := validator.New()
+	err := validate.Struct(campaign)
+	if err == nil {
+		println("nenhum erro")
+	} else {
+		validationsErrors := err.(validator.ValidationErrors)
+		for _, v := range validationsErrors {
+			println(v.StructField() + " is invalid:  " + v.Tag())
+		}
+	}
 }
