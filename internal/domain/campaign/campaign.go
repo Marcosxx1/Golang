ef8ef8c4486ec525f2fa4ef82f8d6e333775eab0
Marcos Alexandre <marcosxx1.ma@gmail.com>
2024-01-13 22:12:12 -0400

    retira criação de NewCampaign

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -1,11 +1,7 @@
 package campaign
 
 import (
-	"Golang/internal/domain/error_handling"
-	"fmt"
 	"time"
-
-	"github.com/rs/xid"
 )
 
 type Contact struct {
@@ -20,26 +16,3 @@ type Campaign struct {
 	Contacts  []Contact `validate:"min=1,max=25,dive"`
 }
 
-func NewCampaign(name string, content string, emails []string) (*Campaign, error) {
-	contacts := make([]Contact, len(emails))
-
-	for indice, valor := range emails {
-		contacts[indice] = Contact{Email: valor}
-	}
-
-	campaign := &Campaign{
-		ID:        xid.New().String(),
-		Name:      name,
-		Content:   content,
-		CreatedAt: time.Now(),
-		Contacts:  contacts,
-	}
-
-	err := error_handling.ValidateStruct(campaign)
-	if err == nil {
-		return campaign, nil
-	}
-	fmt.Println("Validation Error:", err) // Add this line for debugging
-
-	return nil, err
-}
